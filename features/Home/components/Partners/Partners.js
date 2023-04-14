38fe53392aaf6ff86a950aa878a3dbe69f2f6603
Matto <mattoloma@gmail.com>
2023-04-14 22:21:53 +0300

    Box tag removed

--- features/Home/components/Partners/Partners.js
+++ features/Home/components/Partners/Partners.js
@@ -5,16 +5,14 @@ const Partners = () => {
 
     <SimpleGrid columns={partners.length} margin="0 auto" minChildWidth="150px" >
       {partners.map((partner) => (
-        <Box maxWidth="1080px" >
-          <Image alt="partner_photo"
-            src={partner} 
-            key={partner} 
-            alignSelf="center" 
-            padding={{ base:"2rem", sm:"3rem" }}
-            filter="grayScale(2)"
-            opacity="0.5"
-          />
-        </Box>
+        <Image alt="partner_photo"
+          src={partner} 
+          key={partner} 
+          alignSelf="center" 
+          padding={{ base:"2rem", sm:"3rem" }}
+          filter="grayScale(2)"
+          opacity="0.5"
+        />
       ))}
     </SimpleGrid>
   )
