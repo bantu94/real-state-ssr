84deb6cf494c702b9423c38a95b46e0fd28d5e5e
Matto <mattoloma@gmail.com>
2023-04-15 12:22:07 +0300

    property slider responsiveness

--- features/Home/components/featuredProperties/components/PropertySlider/PropertySlider.js
+++ features/Home/components/featuredProperties/components/PropertySlider/PropertySlider.js
@@ -5,11 +5,15 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import PropetyCard from "@/features/data/common/modules/PropertyCards/PropetyCard";
+import { useIsDesktop } from "@/features/data/common/Hooks/useIsDesktop";
 
 function PropertySlider({ featuredProperties }) {
+  // PropertySlider responsiveness 
+  const { isDesktop } = useIsDesktop()
+
   return (
     <Swiper 
-    slidesPerView={3}
+    slidesPerView={ isDesktop ? 3 : 1 }
     spaceBetween={10}
     loop={true}
     centeredSlides={true}
